4a404054b7c528c34a24395ac68f28de98f80ac6
kpnemre <kpnemre51@gmail.com>
2021-03-09 19:01:51 +0300

    Added Heroku Config

--- src/quiz_prj/settings.py
+++ src/quiz_prj/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -138,3 +138,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 CORS_ALLOW_ALL_ORIGINS= True
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
